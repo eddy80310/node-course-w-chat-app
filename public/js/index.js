090191f584f70bf752aed5bf05fb87c454838cc9
eddy80310 <eddy_hung9999@hotmail.com>
2017-03-27 23:28:01 +1000

    test

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server')
 
-    socket.emit('createMessage', {
-        to: 'Andrew', 
-        text: 'Yup, that works for me', 
-    })
 })
 socket.on('disconnect', function () {
     console.log('Disconnected from server')
