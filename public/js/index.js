22a5bb1e5a488cb780e14b6f3d933ac2a6dd028c
eddy80310 <eddy_hung9999@hotmail.com>
2017-03-28 14:17:21 +1000

    timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -8,17 +8,18 @@ socket.on('disconnect', function () {
 })
 
 socket.on('newMessage', function(message) {
-    console.log('New message', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`)
+    li.text(`${message.from} ${formattedTime}: ${message.text}`)
     jQuery('#messages').append(li);
 })
 
 socket.on('newLocationMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `)
+    li.text(`${message.from} ${formattedTime}: `)
     a.attr('href', message.url)
     li.append(a);
     jQuery('#messages').append(li);
@@ -26,12 +27,15 @@ socket.on('newLocationMessage', function(message) {
 
 
 jQuery('#message-form').on('submit', function(e) {
+
+    var messageTextBox = jQuery('[name=message]')
+
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'User', 
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function() {
-
+        messageTextBox.val('');
     })
 })
 
@@ -40,12 +44,17 @@ locationButton.on('click', function() {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser')
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...')
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location')
         alert('Unable to fetch location');
     })
 })
